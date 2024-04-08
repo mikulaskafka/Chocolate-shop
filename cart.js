929fa2bcec4400279b88570d7cb87c92e1c0b633
mikulaskafka <149764581+mikulaskafka@users.noreply.github.com>
2024-04-08 08:06:32 +0200

    Update cart.js

--- cart.js
+++ cart.js
@@ -4,15 +4,37 @@ const addToCartButtons = document.querySelectorAll(
 
 addToCartButtons.forEach((button) => {
   button.addEventListener("click", (event) => {
-    const chocolateCard = event.target.parentElement;
-    const chocolateName =
+    const chocolateCard = 
+      event.target.parentElement;
+    const chocolateCardIndex = 
+      chocolateCard.index;
+    const chocolateName = 
       chocolateCard.querySelector("h3").textContent;
+    const chocolatePrice =
+      chocolateCard.querySelector("p").textContent;
+    const chocolateList = 
+      window.location.pathname.includes("/products.html") ? "Explore our chocolates" : "Featured Chocolates";
 
 window.dataLayer = window.dataLayer || [];
-window.dataLayer.push({
- 'productName': chocolateName
- });
+
+dataLayer.push({
+  event: "add_to_cart",
+  currency: "USD",
+  value: chocolatePrice,
+  ecommerce: {
+    items: [
+      {
+        item_name: chocolateName,
+        item_brand: "The Chocolate Shop",
+        item_category: "Chocolates",
+        item_list_name: chocolateList,
+        index: chocolateCardIndex
+      }
+    ],
+  },
+});
     
     alert(`Added ${chocolateName} to the cart`);
+  
   });
 });
