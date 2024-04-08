5b1f16ed730ea040bdef93f265fe94979b959b1a
mikulaskafka <149764581+mikulaskafka@users.noreply.github.com>
2024-04-08 08:27:19 +0200

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,34 +1,32 @@
-const addToCartButtons = document.querySelectorAll(".add-to-cart-btn");
+document.addEventListener('DOMContentLoaded', () => {
+  const addToCartButtons = document.querySelectorAll(".add-to-cart-btn");
 
-addToCartButtons.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    const chocolateCard = event.target.closest('.chocolate-card');
-    const allChocolateCards = document.querySelectorAll('body .chocolate-card');
-    const chocolateCardsArray = Array.from(allChocolateCards);
-    const chocolateCardIndex = chocolateCardsArray.indexOf(chocolateCard);
-    const chocolateName = chocolateCard.querySelector("h3").textContent;
-    const chocolatePrice = chocolateCard.querySelector("p").textContent.replace(/[^0-9.]/g, '');
-    const chocolateList = window.location.pathname.includes("/products.html") ? "Explore our chocolates" : "Featured Chocolates";
+  addToCartButtons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+      const chocolateCard = button.closest('.chocolate-card');
+      const chocolateName = chocolateCard.querySelector("h3").textContent;
+      const chocolatePrice = chocolateCard.querySelector("p").textContent.replace(/[^0-9.]/g, '');
+      const chocolateList = window.location.pathname.includes("/products.html") ? "Explore our chocolates" : "Featured Chocolates";
 
-    window.dataLayer = window.dataLayer || [];
+      window.dataLayer = window.dataLayer || [];
 
-    dataLayer.push({
-      event: "add_to_cart",
-      currency: "USD",
-      value: parseFloat(chocolatePrice),
-      ecommerce: {
-        items: [
-          {
+      dataLayer.push({
+        event: "add_to_cart",
+        currency: "USD",
+        value: parseFloat(chocolatePrice),
+        ecommerce: {
+          items: [{
             item_name: chocolateName,
             item_brand: "The Chocolate Shop",
             item_category: "Chocolates",
             item_list_name: chocolateList,
-            index: chocolateCardIndex
-          }
-        ],
-      },
-    });
+            // Assuming the index is needed for something specific, though it's not the position in the document.
+            index: index + 1 // 1-based index
+          }]
+        },
+      });
 
-    alert(`Added ${chocolateName} to the cart`);
+      alert(`Added ${chocolateName} to the cart`);
+    });
   });
 });
